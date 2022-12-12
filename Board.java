d6a871f69a040cd5bc156644698bd51263809c81
ngner046 <94244632+ngner046@users.noreply.github.com>
2022-12-12 21:51:50 +0300

    Update Board.java
    Board Class attributes, constructor and its getters are defined. There is no need to define setters. These attributes only change in the class

--- Board.java
+++ Board.java
@@ -1,3 +1,13 @@
 public class Board {
-    
+    // Attributes
+    private Card[] cards;
+    private int size;
+    // Constructor
+    public Board() {
+        this.cards = new Card[52];
+        this.size = 0;
+    }
+    // Methods
+    public Card[] getCards() {return this.cards;}
+    public int getSize() {return this.size;}
 }
