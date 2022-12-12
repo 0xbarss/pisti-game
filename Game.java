ef027086901f6f2500adc2339964c1036ab500fe
ngner046 <94244632+ngner046@users.noreply.github.com>
2022-12-12 21:19:55 +0300

    Update Game.java
    A method for shuffling a deck is defined.

--- Game.java
+++ Game.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Game {
     public static void main(String[] args) {
         // Game Loop
@@ -15,4 +17,16 @@ public class Game {
 		}
 		return cards;
     }
+    public static Card[] shuffleDeck(Random r, Card[] cards) {
+		// Shuffle the deck by Fisher-Yates Shuffle Algorithm
+		Card temp;
+		int randnum;
+		for (int i=cards.length-1; i>0; i--) {
+			randnum = r.nextInt(i+1);
+			temp = cards[i];
+			cards[i] = cards[randnum];
+			cards[randnum] = temp;
+		}
+		return cards;
+	}
 }
