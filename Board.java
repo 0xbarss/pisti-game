9fe1353b8805927a1c92133b4f69a5744a9e5750
ngner046 <94244632+ngner046@users.noreply.github.com>
2022-12-12 22:02:10 +0300

    Update Board.java
    Two methods for adding a card and clearing the board are defined.

--- Board.java
+++ Board.java
@@ -10,4 +10,15 @@ public class Board {
     // Methods
     public Card[] getCards() {return this.cards;}
     public int getSize() {return this.size;}
+    // Special Methods
+	public void addCard(Card card) {
+        // Add a card to the cards array and increase the size 1
+        this.cards[this.size] = card;
+		this.size += 1;
+	}    
+    public void clearBoard() {
+        // Re-assign the attributes
+        this.cards = new Card[52];
+        this.size = 0;
+    }
 }
